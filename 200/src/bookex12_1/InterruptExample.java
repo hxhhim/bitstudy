ae9a63ad85b0ed0998b4358622f71e3551431d4e
hxhhim <67267250+hxhhim@users.noreply.github.com>
2020-08-05 20:47:47 +0900

    Add files via upload

---
+++ 200/src/bookex12_1/InterruptExample.java
@@ -0,0 +1,16 @@
+package bookex12_1;
+
+public class InterruptExample {
+
+	public static void main(String[] args) {
+		Thread thread = new PrintThread2();
+		thread.start();
+		
+		try {Thread.sleep(1000);}catch(InterruptedException e) {}
+		
+		thread.interrupt();
+
+	
+
+}
+}
---
+++ 200/src/bookex12_1/PrintThread2.java
@@ -0,0 +1,17 @@
+package bookex12_1;
+
+public class PrintThread2 extends Thread{
+	public void run() {
+		
+			while(true) {
+				System.out.println("실행중");
+				if(Thread.interrupted()) {
+					break;
+				}
+			}
+		
+		
+		System.out.println("자원 정리");
+		System.out.println("실행 종료");
+	}
+}
