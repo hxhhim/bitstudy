4693b1307d019489c33f12dd3b4d137f476dfd28
hxhhim <67267250+hxhhim@users.noreply.github.com>
2020-11-03 08:51:09 +0900

    스프링 답변형 게시판

---
+++ myJSP/workspace/pro30/src/main/java/com/myspring/pro30/board/controller/BoardControllerImpl.java
@@ -0,0 +1,40 @@
+package com.myspring.pro30.board.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.myspring.pro30.board.service.BoardService;
+import com.myspring.pro30.board.vo.ArticleVO;
+
+@Controller("boardController")
+public class BoardControllerImpl implements BoardController {
+	private static final String CURR_IMAGE_REPO_PATH = "C:\\board\\article_image";
+	@Autowired
+	private BoardService boardService;
+	@Autowired
+	private ArticleVO articleVO;
+	
+	@Override
+	@RequestMapping(value="/board/listArticles.do", method={RequestMethod.GET, RequestMethod.POST})
+	
+	public ModelAndView listArticles(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String viewName = (String)request.getAttribute("viewName");
+		List articlesList = boardService.listArticles();
+		ModelAndView mav = new ModelAndView(viewName);
+		mav.addObject("articlesList", articlesList);
+		return mav;
+	}
+	
+	
+	
+}
